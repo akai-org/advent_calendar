7863d1da1b7da4751087a9a99631bf147fa4f768
Wojciech Młyńczak <wojciechmlynczak1@gmail.com>
2019-11-26 16:19:28 +0100

    Style headers

--- advent_calendar_frontend/src/components/SimpleHeader.vue
+++ advent_calendar_frontend/src/components/SimpleHeader.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="simple-navbar">
     <div class="navbar-link" v-if="currentTask - 1 >= 1">
-      <router-link :to="`/task/${currentTask - 1}`">
+      <router-link :to="`/task/${currentTask - 1}`" class="navbar-link__tag">
         Poprzedni dzień
       </router-link>
     </div>
     <div class="navbar-link">
-      <router-link to="/" >Strona główna</router-link>
+      <router-link to="/" class="navbar-link__tag">Strona główna</router-link>
     </div>
     <div class="navbar-link" v-if="currentTask + 1 <= 24">
-      <router-link :to="`/task/${currentTask + 1}`">
+      <router-link :to="`/task/${currentTask + 1}`" class="navbar-link__tag">
         Następny dzień
       </router-link>
     </div>
@@ -27,11 +27,53 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$link-background: #d39569d0;
+$text-color: #ffffff;
+
 .simple-navbar {
   display: flex;
+  height: 80px;
 
   .navbar-link {
     flex: 1 1 0;
+
+    margin: 0.7rem 0.95rem;
+
+    align-items: center;
+    display: flex;
+    justify-content: center;
+
+    &__tag {
+      border-radius: 8px;
+      box-sizing: border-box;
+      height: 100%;
+      width: 100%;
+
+      align-items: center;
+      display: flex;
+      justify-content: center;
+
+      background: $link-background;
+      color: $text-color;
+      font-size: 1.5rem;
+      text-decoration: none;
+
+      &:visited,
+      &:hover,
+      &:active,
+      &:focus {
+        color: $text-color;
+      }
+
+      &:hover,
+      &:focus {
+        background-color: darken($link-background, 6.5%);
+      }
+
+      &:active {
+        background-color: darken($link-background, 11%);
+      }
+    }
   }
 }
 </style>
